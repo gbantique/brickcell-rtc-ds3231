74ce249d94a9a7f9198401e7a04f6e2259e494f9
gbantique <gbantique@gmail.com>
2023-10-17 09:02:08 +0800

    moved main.ts to test.ts for extension purpose

--- main.ts
+++
@@ -1,9 +0,0 @@
-serial.setBaudRate(BaudRate.BaudRate115200)
-Brickcell.setDate(2, 10, 10, 2023)
-Brickcell.setTime(8, 6, 0)
-basic.forever(function () {
-    serial.writeLine(Brickcell.getDate())
-    serial.writeLine(Brickcell.getTime())
-    serial.writeLine("")
-    basic.pause(2000)
-})
--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+serial.setBaudRate(BaudRate.BaudRate115200)
+Brickcell.setDate(2, 10, 10, 2023)
+Brickcell.setTime(8, 6, 0)
+basic.forever(function () {
+    serial.writeLine(Brickcell.getDate())
+    serial.writeLine(Brickcell.getTime())
+    serial.writeLine("")
+    basic.pause(2000)
+})
